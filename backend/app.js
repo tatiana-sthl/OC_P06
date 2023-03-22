682acc882d55b4075d5a1fa081fa608b21a89339
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-03-22 18:31:44 +0100

    fix bug about plugin mongoose unique validator

--- backend/app.js
+++ backend/app.js
@@ -8,22 +8,23 @@ require('dotenv').config();
 const app = express();
 
 mongoose.connect(process.env.DB_ACCESS,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée ...'));
+    { useNewUrlParser: true,
+     useUnifiedTopology: true 
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-app.use(express.json());
-
-app.use((req, res, next) => {
+app.use((req, res, next) => 
+{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
+
+app.use(express.json());
 
 app.use('/api/auth', userRoutes);
-
 
 module.exports = app;
