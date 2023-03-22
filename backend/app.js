b61c2fcea3ef144900ef71bd9e7551bbd9350b7c
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-03-22 11:23:47 +0100

    try to fix bug connection server

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+require('dotenv').config();
 
 const app = express();
 
+mongoose.connect(process.env.DB_ACCESS,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée ...'));
+
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log('Requête reçue');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-   res.status(201);
-   next(); 
-});
-
-app.use((req, res, next) => {
-    res.json({ message: "Votre requête a bien été reçue" });
-    next()
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès');
-});
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
